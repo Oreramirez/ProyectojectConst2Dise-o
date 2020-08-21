911bed86a468a04c09b014c38a25db1b08bc9afd
oreramirez <oreramirezt@gmail.com>
2020-08-21 06:14:34 -0500

    visualizar pdf
    visualizar pdf

---
+++ APP-MOVIL/app/src/main/java/com/example/rikuwaapp/Vista/PdfViewActivity.java
@@ -0,0 +1,34 @@
+package com.example.rikuwaapp.Vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.example.rikuwaapp.R;
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+
+public class PdfViewActivity extends AppCompatActivity {
+
+    PDFView testpdf;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_view);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+
+        testpdf = findViewById(R.id.pdfviewer);
+        testpdf.fromAsset("protocolo.pdf")
+                .defaultPage(0)
+                .enableAnnotationRendering(true)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .spacing(2)
+                .load();
+    }
+
+    public void VisualizarPdf(){
+
+    }
+}
